2e9c967da1a39add7f4bd72f88cccfb4c70ea78c
ductrong6104 <trongd2002@gmail.com>
2024-10-06 12:11:28 +0700

    update comment code

--- include/utils.h
+++ include/utils.h
@@ -4,7 +4,8 @@
 #include <Arduino.h>
 #include <app_config.h>
 
-void printDeviceRam() {
+void printDeviceRam()
+{
   Serial.print("FREE RAM: ");
   Serial.print(ESP.getFreeHeap() / 1024);
   Serial.print("/");
@@ -13,10 +14,13 @@ void printDeviceRam() {
 }
 
 /* get arr min */
-float ArrMin(float arr[], int size){
+float ArrMin(float arr[], int size)
+{
   float min = arr[0];
-  for(size_t i = 0; i < size; i++){
-    if(arr[i] < min){
+  for (size_t i = 0; i < size; i++)
+  {
+    if (arr[i] < min)
+    {
       min = arr[i];
     }
   }
@@ -24,10 +28,13 @@ float ArrMin(float arr[], int size){
 }
 
 /* get arr max */
-float ArrMax(float arr[], int size){
+float ArrMax(float arr[], int size)
+{
   float max = arr[0];
-  for(size_t i = 0; i < size; i++){
-    if(arr[i] > max){
+  for (size_t i = 0; i < size; i++)
+  {
+    if (arr[i] > max)
+    {
       max = arr[i];
     }
   }
@@ -35,69 +42,80 @@ float ArrMax(float arr[], int size){
 }
 
 /* get arr mean */
-float ArrMean(float arr[], int size){
-    float sum = 0;
-    for (size_t i = 0; i < size; i++) {
-        sum += arr[i];
-    }
-    return (sum / size);
+float ArrMean(float arr[], int size)
+{
+  float sum = 0;
+  for (size_t i = 0; i < size; i++)
+  {
+    sum += arr[i];
+  }
+  return (sum / size);
 }
 
 /* get arr std */
-float ArrStd(float arr[], int size){
+float ArrStd(float arr[], int size)
+{
   float _arrMean = ArrMean(arr, size);
   float sum = 0;
-  for (size_t i = 0; i < size; i++) {
-      float _val = arr[i] - _arrMean;
-        sum += (_val * _val);
-    }
+  for (size_t i = 0; i < size; i++)
+  {
+    float _val = arr[i] - _arrMean;
+    sum += (_val * _val);
+  }
   return sqrt(sum / size);
 }
 
+namespace my_utils
+{
+  /* fourier dft
+   * float timeSignalArr[] : tín hiệu trong miền thời gian
+   * int timeSignalArrSize: kích thước mảng tín hiệu trong miền thời gian
+   * float frequencySignalArr[]: tín hiệu trong miền tần số (sau khi biến đổi)
+   */
+  void DFT(float arr[], int size)
+  {
 
-namespace my_utils{
-    /* fourier dft 
-    * float timeSignalArr[] : tín hiệu trong miền thời gian
-    * int timeSignalArrSize: kích thước mảng tín hiệu trong miền thời gian
-    * float frequencySignalArr[]: tín hiệu trong miền tần số (sau khi biến đổi)
-    */
-    void DFT(float arr[], int size){
+    for (int k = 0; k < size / 2; k++)
+    {
+      RealPart[k] = 0;
+      ImagPart[k] = 0;
 
-    for (int k = 0; k < size / 2; k++) {
-        RealPart[k] = 0;
-        ImagPart[k] = 0;
-
-        for (int n = 0; n < size; n++) {
+      for (int n = 0; n < size; n++)
+      {
         RealPart[k] += arr[n] * cos(2 * PI * n * k / size);
         ImagPart[k] -= arr[n] * sin(2 * PI * n * k / size);
-        }
+      }
     }
-    for (int k = 0; k < size / 2; k++) {
-        FrequencySignalArr[k] = sqrt(RealPart[k] * RealPart[k] + ImagPart[k] * ImagPart[k]);
+    for (int k = 0; k < size / 2; k++)
+    {
+      FrequencySignalArr[k] = sqrt(RealPart[k] * RealPart[k] + ImagPart[k] * ImagPart[k]);
     }
-    }
+  }
 
-    /*
-    * get max frequency
-    */
-    float GetMaxFrequency(float arr[], int size){
-        // GetFrequencies();
-        DFT(arr, size);
-        int maxFPos = 1;
-        float maxF = FrequencySignalArr[maxFPos];
-        // Serial.println("CCC:::" + String(FrequencySignalArr[0]));
-        // Serial.println("CCC:::" + String(FrequencySignalArr[1]));
-        // Serial.println("CCC:::" + String(FrequencySignalArr[2]));
-        for (size_t k = 1; k <= (int)MAX_FREQUENCY_POS; k++) {
-        if(maxF < FrequencySignalArr[k]){
-            maxFPos = k;
-            maxF = FrequencySignalArr[k];
-        }
-        }
-        float result = (float)(maxFPos * FREQUENCY_STEP);
-        // Serial.println(result);
-        return result;
+  /*
+   * get max frequency
+   */
+  float GetMaxFrequency(float arr[], int size)
+  {
+    // GetFrequencies();
+    DFT(arr, size);
+    int maxFPos = 1;
+    float maxF = FrequencySignalArr[maxFPos];
+    // Serial.println("CCC:::" + String(FrequencySignalArr[0]));
+    // Serial.println("CCC:::" + String(FrequencySignalArr[1]));
+    // Serial.println("CCC:::" + String(FrequencySignalArr[2]));
+    for (size_t k = 1; k <= (int)MAX_FREQUENCY_POS; k++)
+    {
+      if (maxF < FrequencySignalArr[k])
+      {
+        maxFPos = k;
+        maxF = FrequencySignalArr[k];
+      }
     }
-    }
+    float result = (float)(maxFPos * FREQUENCY_STEP);
+    // Serial.println(result);
+    return result;
+  }
+}
 
 #endif
